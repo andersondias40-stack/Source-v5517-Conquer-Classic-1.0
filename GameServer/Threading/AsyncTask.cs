fa58a45d652e57406353bc35256da68d750f120a
BlackoutlFKZ <andersondias40@gmail.com>
2026-07-24 04:22:22 -0300

    Add files via upload

---
+++ GameServer/Threading/AsyncTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace COServer.Threading
+{
+    internal static class Repeat
+    {
+        public static Task Interval(
+            TimeSpan pollInterval,
+            Action action,
+            CancellationToken token)
+        {
+            return Task.Factory.StartNew(
+                () =>
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+
+                        if (token.WaitCancellationRequested(pollInterval))
+                            break;
+
+                        action();
+                    }
+                }, token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+    }
+
+    static class CancellationTokenExtensions
+    {
+        public static bool WaitCancellationRequested(
+            this CancellationToken token,
+            TimeSpan timeout)
+        {
+            return token.WaitHandle.WaitOne(timeout);
+        }
+    }
+}
