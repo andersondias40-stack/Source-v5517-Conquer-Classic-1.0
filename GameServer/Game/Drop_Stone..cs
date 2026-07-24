18417dde2a73338d9b5560674e018da7bb912adb
BlackoutlFKZ <andersondias40@gmail.com>
2026-07-24 04:42:06 -0300

    Add files via upload

---
+++ GameServer/Game/Drop_Stone..cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COServer.Game
+{
+    public static class Drop_Stone
+    {
+        public static readonly uint[] helmets = { 111005, 111006, 111008, 113004, 113035, 114003, 114006, 114008, 114025, 114028, 142003 };  
+        public static readonly uint[] armors = { 130003, 130003, 130008, 130063, 130013, 131002, 131073, 133003, 133043, 133028, 133033 };
+        public static readonly uint[] swords = { 420023, 420033, 420050, 420058, 420070, 420023, 420023 };
+        public static readonly uint[] TaoRobe = { 134003, 134008, 134063, 134003, 134025, 134030 };
+        public static readonly uint[] bows = { 500013, 500043, 500013, 500043, 500046, 500113, 500173, 500053, 500043, 500013 };
+        public static readonly uint[] Headband = { 141003, 141023, 141028, 141093 };
+        public static readonly uint[] clubs = { 480003, 480003, 480008, 480023, 480023, 480070, 480133, 480166 };
+        public static readonly uint[] BackSword = { 421023, 421028, 420042, 421080, 421100, 421143, 421148, 421023 };
+        public static readonly uint[] blades = { 410023, 410080, 410088, 410023, 410050, 410080 };
+        public static readonly uint[] wands = { 561023, 561023, 561043, 561110, 561136, 580023, 580033, 580133 };
+        public static readonly uint[] sceptres = { 481023, 481023, 481100, 481070, 481068 };
+        public static readonly uint[] Dagger = { 490023, 490043, 490023, 490043, 481068, 490068, 490060 };
+        public static readonly uint[] glaives = { 510023, 510050, 510023, 510058, 510223 };
+        public static readonly uint[] Hook = { 430050, 430060, 430070, 430048 };
+        public static readonly uint[] Whip = { 440023, 440060, 440076, 440090 };
+        public static readonly uint[] Axe = { 450023, 450050, 450090, 450066, 530023, 530100, 530106, 530173 };
+        public static readonly uint[] LongHammer = { 540023, 540033, 540023, 540143, 540177 };
+        public static readonly uint[] shields = { 900010, 900020, 900020, 900033, 900048, 900043, 900073 };
+        public static readonly uint[] Spear = { 560023, 560023, 560080, 560088, 560023 };
+        public static readonly uint[] boots = { 160013, 160018, 160033, 160070 };
+        public static readonly uint[] necklaces = { 120003, 120003, 120008, 120080, 120090 };
+        public static readonly uint[] rings = { 150003, 150008, 150033, 150153, 151013, 151018, 151173 };
+        public static readonly uint[] bracelets = { 152013, 152016, 152080, 152163 };
+        public static readonly uint[] Earring = { 117003, 117008, 117075, 117078 };
+        public static readonly uint[] Coronet = { 118003, 118008, 118023, 118028 };
+        public static readonly uint[] Bag = { 121023, 121023, 121028, 121153, 121153, 121158, 121183, 121183 };
+    }
+}
