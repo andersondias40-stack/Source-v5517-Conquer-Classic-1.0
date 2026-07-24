4735c02c4c428a9975b9a69b8e81450d9a8c9617
BlackoutlFKZ <andersondias40@gmail.com>
2026-07-24 04:21:16 -0300

    Add files via upload

---
+++ GameServer/Global.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COServer
+{
+    public class Global
+    {
+        public const double
+        LUCKY_BLUE_MOUSE_RATE = 0,
+        LUCKY_TIME_EXP_RATE = 0,
+        LUCKY_TIME_PLUS_RATE = 0,
+        LUCKY_TIME_BONUS_SOCKET_RATE = 0,//This is ADDED to the existing socket rate
+        LUCKY_TIME_CRIT_RATE_RANGED = 0,//One in 20 monsters hit with lucky time will take double dmg
+        LUCKY_TIME_CRIT_RATE_PHYSICAL = 0,//One in 20 monsters hit with lucky time will take double dmg
+        LUCKY_TIME_CRIT_RATE_MAGIC = 0,//One in 20 monsters hit with lucky time will take double dmg
+        LUCKY_TIME_CRIT_RATE_MONSTER = 0,
+        MINING_DROP_GEMS = 0.2,            // 20% porcentagem de gems normal.
+        MINING_DROP_GEMS_REFIND = 0.10,     // 10% porcentagem de gems refine
+        MINING_DROP_GEMS_SUPER = 0.02,     //porcentagem de gems. super
+        MINING_DROP_EUXENITE = 0.20;     // 15 % porcentagem de Euxenite Ore
+    }
+}
