4a7425518ed9388ff4a11e52f8b209225a7e4d0a
BlackoutlFKZ <andersondias40@gmail.com>
2026-07-24 04:41:42 -0300

    Add files via upload

---
+++ GameServer/Game/MsgTournaments/Events/LastMan.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COServer.Game.MsgTournaments
+{
+    public class LastMan
+    {
+        public const uint Map = 1844;
+        private ProcesType Mode;
+        private DateTime FinishTimer = new DateTime();
+        private string Title = "[ LastMan ]";
+        public uint WinnerUID = 0;
+
+        public LastMan()
+        {
+            Mode = ProcesType.Dead;
+            if (!Program.OutMap.Contains(Map))
+                Program.OutMap.Add(Map);
+            if (!Program.FreePkMap.Contains(Map))
+                Program.FreePkMap.Add(Map);
+        }
+
+        public void Open()
+        {
+            if (Mode == ProcesType.Dead)
+            {
+                FinishTimer = DateTime.Now.AddMinutes(1);
+                Mode = ProcesType.Alive;
+                MsgSchedules.SendSysMesage("" + Title + " has started in TwinCity", MsgServer.MsgMessage.ChatMode.Center, MsgServer.MsgMessage.MsgColor.yellow);
+            }
+        }
+
+        public void CheckUp()
+        {
+            if (Mode == ProcesType.Alive)
+            {
+                if (DateTime.Now > FinishTimer)
+                {
+                    Mode = ProcesType.Dead;
+                }
+            }
+            if (DateTime.Now.Minute == 50 && DateTime.Now.Second < 2)
+            {
+                if (Mode == ProcesType.Dead)
+                {
+                    FinishTimer = DateTime.Now.AddMinutes(1);
+                    Mode = ProcesType.Alive;
+                    MsgSchedules.SendSysMesage("" + Title + " has started in TwinCity", MsgServer.MsgMessage.ChatMode.Center, MsgServer.MsgMessage.MsgColor.red);
+                }
+            }
+        }
+
+        public bool AllowJoin(Client.GameClient user, ServerSockets.Packet stream)
+        {
+            if (Mode == ProcesType.Alive)
+            {
+                ushort x = 0;
+                ushort y = 0;
+                Database.Server.ServerMaps[Map].GetRandCoord(ref x, ref y);
+                user.Teleport(x, y, Map);
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsFinished() { return Mode == ProcesType.Dead; }
+
+        public bool TheLastPlayer()
+        {
+            return Database.Server.GamePoll.Values.Where(p => p.Player.Map == Map && p.Player.Alive).Count() == 1;
+        }
+
+        public void GiveReward(Client.GameClient client, ServerSockets.Packet stream)
+        {
+            WinnerUID = client.Player.UID;
+
+            // Quantidade de silver que o jogador vai receber
+            uint silverAmount = 50000; //  50 mil silver
+
+            
+            client.SendSysMesage($"You received {silverAmount} silver.", MsgServer.MsgMessage.ChatMode.System, MsgServer.MsgMessage.MsgColor.red);
+            MsgSchedules.SendSysMesage($"{client.Player.Name} has won {Title} and received {silverAmount} silver!", MsgServer.MsgMessage.ChatMode.TopLeftSystem, MsgServer.MsgMessage.MsgColor.white);
+
+           
+            Program.DiscordAPIwinners.Enqueue($"``[{client.Player.Name}] Won {Title} and received {silverAmount} silver.``");
+
+            
+            client.Player.Money += silverAmount;
+
+            
+            client.Player.HitPoints = (int)client.Status.MaxHitpoints;
+            client.Teleport(438, 387, 1002);
+        }
+
+        public void AddTop(Client.GameClient client)
+        {
+            if (WinnerUID == client.Player.UID)
+                client.EffectStatus.Add("toplast", DateTime.Now.AddHours(12));
+
+            foreach (var user in Database.Server.GamePoll.Values)
+            {
+                if (user.Player.UID != WinnerUID)
+                    user.EffectStatus.Remove("toplast");
+            }
+        }
+    }
+}
+
---
+++ GameServer/Game/MsgTournaments/KillerSystem.cs
@@ -0,0 +1,96 @@
+﻿using COServer.Game.MsgServer;
+
+namespace COServer.Game.MsgTournaments
+{
+    public class KillerSystem
+    {
+        public enum KillFlag : byte
+        {
+            None = 0,
+            FirstBloodKill = 1,
+            DoubleKill = 2,
+            TripleKill = 3,
+            UnStoppable = 4
+        }
+
+        public static string GetSound(KillerSystem user)
+        {
+            switch (user.Flag)
+            {
+                case KillFlag.FirstBloodKill: return "sound\\fc1.wav";
+                case KillFlag.DoubleKill: return "sound\\fc2.wav";
+                case KillFlag.TripleKill: return "sound\\fc3.wav";
+                case KillFlag.UnStoppable: return "sound\\fc4.wav";
+            }
+            return "none";
+        }
+
+        public KillFlag Flag = KillFlag.None;
+        public bool Dead = false;
+        public uint UID = 0;
+
+        public void CheckDead(uint _UID)
+        {
+            if (_UID == UID)
+                Dead = true;
+        }
+
+        public KillerSystem()
+        {
+        }
+
+        public void Update(Client.GameClient client)
+        {
+            if (UID == 0)
+                UID = client.Player.UID;
+
+            if (UID == client.Player.UID && Flag < KillFlag.UnStoppable && Dead == false)
+            {
+                switch (Flag)
+                {
+                    case KillFlag.None: Flag = KillFlag.FirstBloodKill; break;
+                    case KillFlag.FirstBloodKill: Flag = KillFlag.DoubleKill; break;
+                    case KillFlag.DoubleKill: Flag = KillFlag.TripleKill; break;
+                    case KillFlag.TripleKill: Flag = KillFlag.UnStoppable; break;
+                }
+                Send(client);
+            }
+        }
+
+        public void Send(Client.GameClient client)
+        {
+            using (var rec = new ServerSockets.RecycledPacket())
+            using (var rec2 = new ServerSockets.RecycledPacket())
+            {
+                var stream = rec.GetStream();
+                var stream2 = rec2.GetStream();
+
+                // Pacote de som
+                Game.MsgServer.MsgStringPacket packet = new Game.MsgServer.MsgStringPacket
+                {
+                    ID = MsgServer.MsgStringPacket.StringID.Sound,
+                    Strings = new string[2]
+                };
+                packet.Strings[0] = GetSound(this);
+                packet.Strings[1] = "1";
+                stream = stream.StringPacketCreate(packet);
+
+                // Mensagem de texto
+                var msg = new MsgMessage($"{this.Flag} {client.Player.Name}.",
+                                         MsgMessage.MsgColor.red,
+                                         MsgMessage.ChatMode.System);
+                stream2 = msg.GetArray(stream2);
+
+                foreach (var user in Database.Server.GamePoll.Values)
+                {
+                    if (user.Player.Map == client.Player.Map &&
+                        user.Player.DynamicID == client.Player.DynamicID)
+                    {
+                        user.Send(stream);
+                        user.Send(stream2);
+                    }
+                }
+            }
+        }
+    }
+}
